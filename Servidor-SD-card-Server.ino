e2aab64467e7bbced6c659922d0b77eb50148a7b
Jefferson Carlos Lima <92160349+JeffersonCarlosLima@users.noreply.github.com>
2022-09-08 16:48:15 -0300

    Add files via upload

---
+++ Servidor-SD-card-Server.ino
@@ -0,0 +1,79 @@
+#include <SPI.h>
+#include <String.h>
+#include <Ethernet.h>
+#include <SD.h>
+
+// MAC address from Ethernet shield sticker under board
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x9B, 0x36 }; //physical mac address
+byte ip[] = { 192, 168, 11, 188 }; // ip in lan
+byte gateway[] = { 192, 168, 11, 1 }; // internet access via router
+byte subnet[] = { 255, 255, 255, 0 }; //subnet mask
+EthernetServer server(80); //server port
+
+
+File webFile;
+
+void setup()
+{
+    Ethernet.begin(mac, ip);  // initialize Ethernet device
+    server.begin();           // start to listen for clients
+    Serial.begin(9600);       // for debugging
+    
+    // initialize SD card
+    Serial.println("Initializing SD card...");
+    if (!SD.begin(4)) {
+        Serial.println("ERROR - SD card initialization failed!");
+        return;    // init failed
+    }
+    Serial.println("SUCCESS - SD card initialized.");
+    // check for index.htm file
+    if (!SD.exists("INDEX~1.HTM")) {
+        Serial.println("ERROR - Can't find index.html file!");
+        return;  // can't find index file
+    }
+    Serial.println("Arquivo index.html encontrado.");
+}
+
+void loop()
+{
+    EthernetClient client = server.available();  // try to get client
+
+    if (client) {  // got client?
+        boolean currentLineIsBlank = true;
+        while (client.connected()) {
+            if (client.available()) {   // client data available to read
+                char c = client.read(); // read 1 byte (character) from client
+                // last line of client request is blank and ends with \n
+                // respond to client only after last line received
+                if (c == '\n' && currentLineIsBlank) {
+                    // send a standard http response header
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-Type: text/html");
+                    client.println("Connection: close");
+                    client.println();
+                    // send web page
+                    webFile = SD.open("INDEX~1.HTM");        // open web page file
+                    if (webFile) {
+                        while(webFile.available()) {
+                            client.write(webFile.read()); // send web page to client
+                        }
+                        webFile.close();
+                    }
+                    break;
+                }
+                // every line of text received from the client ends with \r\n
+                if (c == '\n') {
+                    // last character on line of received text
+                    // starting new line with next character read
+                    currentLineIsBlank = true;
+                } 
+                else if (c != '\r') {
+                    // a text character was received from client
+                    currentLineIsBlank = false;
+                }
+            } // end if (client.available())
+        } // end while (client.connected())
+        delay(1);      // give the web browser time to receive the data
+        client.stop(); // close the connection
+    } // end if (client)
+}
